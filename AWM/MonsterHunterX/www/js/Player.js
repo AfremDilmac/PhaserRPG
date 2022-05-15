02ae962399e33955d5edf5ef756287e64e4d9fee
AfremDilmac <afrem.dilmac@student.odisee.be>
2022-05-15 19:10:45 +0200

    repositioning joystick + fix map

--- AWM/MonsterHunterX/www/js/Player.js
+++ AWM/MonsterHunterX/www/js/Player.js
@@ -76,7 +76,7 @@ class Player extends Entity {
 
         //Joystick plugin
         this.joyStick = scene.plugins.get('rexvirtualjoystickplugin').add(scene, {
-            x: 560,
+            x: 660,
             y: 300,
             radius: 25,
             base: scene.add.circle(0, 0, 30, 0x888888, 0.5),
