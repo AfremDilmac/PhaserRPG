7ac19202716db9501d5b2083f4c24aacbd4f6b64
AdamAzzouz <71710942+AdamAzzouz@users.noreply.github.com>
2022-05-19 21:54:07 +0200

    login btn + local storage

--- AWM/MonsterHunterX/www/js/firestore.js
+++ AWM/MonsterHunterX/www/js/firestore.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import { getFirestore, collection, getDocs, updateDoc, addDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"; 
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAeBjdQt26lGPxqiuUeQvDGLiFfbEbYYS8",
@@ -9,12 +11,15 @@ const firebaseConfig = {
     storageBucket: "monsterhunter-d7680.appspot.com",
     messagingSenderId: "338059376056",
     appId: "1:338059376056:web:a1bb36e87101c4f2598b4d"
-  };
+};
 
-  initializeApp(firebaseConfig)
-  const db = getFirestore()
-  //collection ref (users)
-  const colRef = collection(db, 'users')
+//Initialize Firebase
+   const app = initializeApp(firebaseConfig);
+   const database = getDatabase(app);
+   const auth = getAuth();
+
+   const db = getFirestore()
+   const colRef = collection(db, 'users')
 
   //array van docs uitloggen 
   getDocs(colRef)
@@ -28,6 +33,119 @@ const firebaseConfig = {
 	  .catch(err => {
 		  console.log(err.message);
 	  })
+
+
+	  const gotoLogin = document.getElementById('gotoLogin');
+	  const signupBtn = document.getElementById('signUp');
+
+	  gotoLogin.style.display = 'none'
+	  	// nieuwe doc toevoegen in firestore bij signup 
+	const sigupForm = document.querySelector('.left')
+
+	sigupForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		var email = document.getElementById('email').value;
+		var password = document.getElementById('password').value;
+		var username = document.getElementById('username').value;
+		var lblError = document.getElementById('errormsg-signup')
+  
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in 
+				const user = userCredential.user;
+
+				set(ref(database, 'users/' + user.uid),{
+					username: username,
+					email: email
+				})
+				console.log(user.uid); // ID VAN AUTH
+				localStorage.setItem("ID", user.uid)
+				let ident = localStorage.getItem('ID')
+
+				setDoc(doc(db, "users", ident), {
+					name: username,
+					gold: 0,
+					health: 50,
+					positionX: 90,
+					positionY: 3159,			
+				});
+			
+			})
+				signupBtn.style.display = 'none'
+				gotoLogin.style.display = 'block'
+
+				lblError.innerHTML='user created!'
+				
+			})
+			.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+
+			lblError.innerHTML= errorCode
+			// ..
+			});
+
+
+
+
+
+let i = localStorage.getItem('ID')
+const docRef = doc(db, 'users', i)
+
+let a = localStorage.getItem('GOLD:')
+let b = localStorage.getItem('health')
+let c = localStorage.getItem('positionX')
+let d = localStorage.getItem('positionY')
+
+docRef.updateDoc(docRef, {
+	gold: a,
+	health: b,
+	positionX: c,
+	positionY: d,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // nieuwe doc toevoegen in firestore bij signup 
@@ -56,18 +174,31 @@ const firebaseConfig = {
 
 
 
+//  login.addEventListener('click',(e)=>{
+//    var email = document.getElementById('email').value;
+//    var password = document.getElementById('password').value;
 
-  
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
-//   
+//       signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         // Signed in 
+//         const user = userCredential.user;
+
+//         const dt = new Date();
+//          update(ref(database, 'users/' + user.uid),{
+//           last_login: dt,
+//         })
+
+//          alert('User loged in!');
+//         // ...
+//       })
+//       .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+
+//         alert(errorMessage);
+//   });
+
+//  });
+
+
+
